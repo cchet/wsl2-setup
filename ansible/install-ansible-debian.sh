dc0c15698cb41317a3736064a163d595beb76202
cchet <thomas.herzog@gepardec.com>
2020-05-30 18:09:25 +0200

    - Commit forgotten files

--- ansible/install-ansible-debian.sh
+++ ansible/install-ansible-debian.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set -ue
+
 ANSIBLE_VERSION='2.9.6+dfsg-1~bpo10+1'
 
 apt-get update
-apt-get install -y ansible=${ANSIBLE_VERSION} python-pip python3-pip
+apt-get install -y ansible=${ANSIBLE_VERSION} python-pip python-apt python3-pip python3-apt 
 pip install pexpect
 pip3 install pexpect
+
+echo "[targets]
+localhost ansible_connection=local ansible_python_interpreter=/usr/bin/python2" > /etc/ansible/hosts
